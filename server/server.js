0b8eed185e9b3cd10aca22b1f3039ae4a5e272c5
LesegoLSG <lesegomhlongo78@gmail.com>
2024-11-12 06:05:52 -0600

    Connect to mongo database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(
+   process.env.MONGO
+).then(() => {console.log('Database is connected')}
+).catch((err) =>{
+    console.log("Bad credentials", err);
+})
 
 const app = express();
 
 app.listen(8000, () =>{
-    console.log('server is running on port 8000 ');
+    console.log('server is running on port 8000');
 })
