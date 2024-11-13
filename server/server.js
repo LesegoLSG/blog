c8844431f32013de86048b0982b55c9897c4af35
LesegoLSG <lesegomhlongo78@gmail.com>
2024-11-13 01:47:50 -0600

    Added controller and route with test api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -15,4 +16,7 @@ const app = express();
 
 app.listen(8000, () =>{
     console.log('server is running on port 8000 happy');
-})
+});
+
+//Test
+app.use('/api/user', userRoutes);
