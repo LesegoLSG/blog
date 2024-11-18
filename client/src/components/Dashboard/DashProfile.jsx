d306e660c2dcadac7a24e2825228b8f6b7353354
LesegoLSG <lesegomhlongo78@gmail.com>
2024-11-17 23:00:02 -0600

    Completed image upload functionality

--- client/src/components/Dashboard/DashProfile.jsx
+++ client/src/components/Dashboard/DashProfile.jsx
@@ -6,14 +6,30 @@ import {
   validateLettersOnly,
   validatePhoneNumber,
 } from "../Reusables/Validations/InputValidation";
+import { useNavigate } from "react-router-dom";
 import { signout } from "../../redux/user/userSlice";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const DashProfile = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadingProgress, setImageFileUploadProgress] =
+    useState(null);
+  const [imageFileUploadingError, setImageFileUploadError] = useState(null);
   const filePickerRef = useRef();
+
+  console.log("Checking", imageFileUploadingProgress, imageFileUploadingError);
 
   const [formData, setFormData] = useState({
     firstName: currentUser.firstName,
@@ -29,7 +45,34 @@ const DashProfile = () => {
     }
   }, [imageFile]);
 
-  const uploadImage = async () => {};
+  const uploadImage = async () => {
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Could not upload an image (file must be less than 2MB)"
+        );
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
+  };
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -74,14 +117,44 @@ const DashProfile = () => {
             ref={filePickerRef}
             className="hidden"
           />
-          <div className="flex flex-col justify-center items-center">
+          <div className="relative flex flex-col justify-center items-center">
+            {imageFileUploadingProgress && (
+              <CircularProgressbar
+                value={imageFileUploadingProgress}
+                text={`${imageFileUploadingProgress}%`}
+                strokeWidth={5}
+                styles={{
+                  root: {
+                    width: "100%",
+                    height: "100%",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                  },
+                  path: {
+                    stroke: `rgba(62,152,199, ${
+                      imageFileUploadingProgress / 100
+                    })`,
+                  },
+                }}
+              />
+            )}
             <img
               src={imageFileUrl || currentUser.profilePicture}
               alt="User"
-              className="w-36 h-36 rounded-full cursor-pointer"
+              className={`w-36 h-36 rounded-full cursor-pointer ${
+                imageFileUploadingProgress &&
+                imageFileUploadingProgress < 100 &&
+                "opacity-50"
+              }`}
               onClick={() => filePickerRef.current.click()}
             />
           </div>
+          {imageFileUploadingError && (
+            <p className="text-red-600 text-sm font-semibold text-center">
+              {imageFileUploadingError}
+            </p>
+          )}
 
           <div className="w-full space-y-4">
             <InputField
@@ -125,6 +198,14 @@ const DashProfile = () => {
               <span>Delete account</span>
               <span onClick={handleSignOut}>Sign out</span>
             </div>
+            {currentUser.isAdmin && (
+              <button
+                className="w-full bg-blue-400 p-2"
+                onClick={() => navigate("/create-post")}
+              >
+                Create a post
+              </button>
+            )}
           </div>
         </form>
       </div>
