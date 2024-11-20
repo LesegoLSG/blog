99dc9614f86ddceea70c3975d8731c42760347ad
LesegoLSG <lesegomhlongo78@gmail.com>
2024-11-19 22:43:48 -0600

    Created upload image functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,16 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "../components/Reusables/InputFields/InputField";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import {
+  getStorage,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const CreatePost = () => {
+  const [file, setFile] = useState(null);
+  const [imageFileUploadingProgress, setImageFileUploadProgress] =
+    useState(null);
+  const [imageFileUploadingError, setImageFileUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const handleImageUpload = async (e) => {
+    e.preventDefault();
+    try {
+      if (!file) {
+        setImageFileUploadError("Please select an image");
+        return;
+      }
+
+      setImageFileUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + "-" + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageFileUploadProgress(progress.toFixed(0));
+          console.log("Upload progress:", imageFileUploadingProgress, progress);
+        },
+        (error) => {
+          setImageFileUploadError(
+            "Could not upload an image (file must be less than 2MB)"
+          );
+          setImageFileUploadProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log(downloadURL);
+            setFormData({ ...formData, image: downloadURL });
+            setImageFileUploadProgress(null);
+            setImageFileUploadError(null);
+          });
+        }
+      );
+    } catch (error) {
+      setImageFileUploadError("Error uploading an image, please try again...");
+      setImageFileUploadProgress(null);
+    }
+  };
   return (
     <section className="max-w-4xl mx-auto p-4 min-h-screen">
       <h1 className="text-lg font-bold text-center my-6">Create a post</h1>
       <form className="flex flex-col gap-4">
         <div className="flex flex-col md:flex-row justify-center items-center gap-4">
           {/* Title input */}
-          <InputField placeholder="Title" className="flex-1" />
+          {/* <InputField placeholder="Title" className="flex-1" /> */}
           {/* Select programming language input */}
           <select
             id="language"
@@ -29,10 +85,39 @@ const CreatePost = () => {
           </select>
         </div>
         <div className="flex justify-between items-center">
-          <input type="file" accept="image/*" />
-          <button className="bg-blue-400 p-2">Upload image</button>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <button
+            className="bg-blue-400 p-2"
+            disabled={imageFileUploadingProgress}
+            onClick={handleImageUpload}
+          >
+            {imageFileUploadingProgress ? (
+              <div className="w-16 h-16">
+                <CircularProgressbar
+                  value={imageFileUploadingProgress}
+                  text={`${imageFileUploadingProgress || 0}%`}
+                />
+              </div>
+            ) : (
+              "Upload Image"
+            )}
+          </button>
         </div>
-        return{" "}
+        {/* Image error */}
+        {imageFileUploadingError && (
+          <p className="text-red-600 text-sm font-semibold text-center">
+            {imageFileUploadingError}
+          </p>
+        )}
+        {/* Display an image if formData.image is not null */}
+        {formData.image && (
+          <img src={formData.image} alt="image-display" className="h-72" />
+        )}
+
         <ReactQuill
           theme="snow"
           placeholder="Content"
--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -11,7 +11,7 @@ export const create = async (req,res,next) =>{
 
     const slug = req.body.title
         .split(' ')
-        .join('_').toLowerCase().replace(/[^a-zA-Z0-9-]/g,'-');
+        .join('_').toLowerCase().replace(/[^a-zA-Z0-9-]/g,'');
 
     const newPost = new Post({
         ...req.body,
