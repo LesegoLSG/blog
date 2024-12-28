9a382876a59b9ac1c974e0b00bb035438d993b85
LesegoLSG <lesegomhlongo78@gmail.com>
2024-12-27 18:57:46 -0600

    Added more categories

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -131,14 +131,53 @@ const CreatePost = () => {
             required={true}
           >
             <option value="" disabled selected>
-              Select a language
+              Select a category
             </option>
-            <option value="javascript">JavaScript</option>
-            <option value="python">Python</option>
-            <option value="java">Java</option>
-            <option value="csharp">C#</option>
-            <option value="php">PHP</option>
-            <option value="ruby">Ruby</option>
+            <option value="html">HTML</option>
+            <option value="css">CSS</option>
+            <option value="typescript">TypeScript</option>
+            <option value="nodejs">Node.js</option>
+            <option value="angular">Angular</option>
+            <option value="vuejs">Vue.js</option>
+            <option value="flutter">Flutter</option>
+            <option value="kotlin">Kotlin</option>
+            <option value="swift">Swift</option>
+            <option value="docker">Docker</option>
+            <option value="kubernetes">Kubernetes</option>
+            <option value="data-science">Data Science</option>
+            <option value="machine-learning">Machine Learning</option>
+            <option value="cloud-computing">Cloud Computing</option>
+            <option value="cybersecurity">Cybersecurity</option>
+            <option value="blockchain">Blockchain</option>
+            <option value="devops">DevOps</option>
+            <option value="ui-ux">UI/UX Design</option>
+            <option value="graphql">GraphQL</option>
+            <option value="django">Django</option>
+            <option value="flask">Flask</option>
+            <option value="laravel">Laravel</option>
+            <option value="aspnet">ASP.NET</option>
+            <option value="game-development">Game Development</option>
+            <option value="mobile-development">Mobile Development</option>
+            <option value="software-testing">Software Testing</option>
+            <option value="career-tips">Career Tips</option>
+            <option value="open-source">Open Source</option>
+            <option value="startup">Startup Culture</option>
+            <option value="productivity-tools">Productivity Tools</option>
+            <option value="api-development">API Development</option>
+            <option value="testing-tools">Testing Tools</option>
+            <option value="cloud-platforms">Cloud Platforms</option>
+            <option value="big-data">Big Data</option>
+            <option value="web3">Web3</option>
+            <option value="augmented-reality">Augmented Reality</option>
+            <option value="virtual-reality">Virtual Reality</option>
+            <option value="quantum-computing">Quantum Computing</option>
+            <option value="personal-development">Personal Development</option>
+
+            <option value="Artificial Intelligence">
+              Artificial Intelligence
+            </option>
+            <option value="Conference">Conference</option>
+            <option value="Other">Other</option>
           </select>
         </div>
         <div className="flex justify-between items-center">
