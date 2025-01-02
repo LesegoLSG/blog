c29ab07748c4c42cdeb406438d9f27c1c38a4316
LesegoLSG <lesegomhlongo78@gmail.com>
2025-01-02 15:47:50 -0600

    Modified content details

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaLightbulb, FaUsers, FaCode, FaRocket } from "react-icons/fa";
 import DarkBannerImage from "../assets/HomePageImage/DarkBannerImage.png";
 import Team from "../assets/AboutImages/Team.jpg";
+import ProfileImageLarge from "../assets/ProfileImageLarge.png";
 import { useSelector } from "react-redux";
+import Popup from "../components/Reusables/PopUp";
 
 const AboutUs = () => {
   const { theme } = useSelector((state) => state.theme);
+  const [isPopupOpen, setPopupOpen] = useState(false);
+  const [popupMessage, setPopupMessage] = useState("");
+  const [popupTitle, setPopupTitle] = useState("");
+
+  // Open pop up
+  const handleButtonClick = (title, message) => {
+    setPopupTitle(title);
+    setPopupMessage(message);
+    setPopupOpen(true);
+  };
+
+  // Close pop up
+  const closePopup = () => setPopupOpen(false);
+
   return (
     <section
       className={`w-full py-16 ${
@@ -20,49 +36,48 @@ const AboutUs = () => {
           <div className="absolute inset-0 z-0 overflow-hidden border  border-accent rounded-lg p-1">
             <img
               src={DarkBannerImage}
-              alt="Our Story"
+              alt="My Story"
               className="w-full h-full object-cover opacity-60 rounded-lg"
             />
           </div>
-          <div className="relative z-10 text-center text-white">
-            <h2 className="text-4xl font-extrabold mb-4">How We Got Started</h2>
+          <div className="relative z-10 text-center text-white px-1.5 md:p-0">
+            <h2 className="text-4xl font-extrabold mb-4">How I Got Started</h2>
             <p className="text-lg mb-8 max-w-3xl mx-auto">
-              We started with one simple goal: to provide tech professionals
-              with a place to stay updated and inspired. Our journey began with
-              a small group of developers, each bringing their unique skills to
-              create a dynamic platform. The result is a blog that serves as a
-              bridge between the latest trends in programming and the growing
-              community of developers.
+              My passion for technology began with a simple goal: to create
+              solutions that make life easier. Over the years, I have honed my
+              skills in development, design, and problem-solving, which have
+              allowed me to build impactful projects and continue growing in the
+              ever-evolving tech landscape.
             </p>
           </div>
         </div>
 
-        {/* People Section */}
+        {/* About me Section */}
         <div className="flex flex-col-reverse md:flex-row gap-16 py-24">
-          <div className="flex-1">
-            <h3 className="text-3xl font-extrabold  mb-4">Meet the Team</h3>
+          <div className="flex-1 px-1.5 md:px-0">
+            <h3 className="text-3xl font-extrabold  mb-4">Meet Me</h3>
             <p className="text-lg  mb-6">
-              We are a diverse team of developers, designers, and content
-              creators passionate about making complex tech topics accessible.
-              Together, we bring a wide range of experiences to create content
-              that speaks to developers of all levels.
+              I am a passionate developer with a keen interest in creating
+              innovative solutions. My expertise spans various areas of
+              technology, and I am constantly striving to expand my knowledge
+              and contribute meaningfully to the tech community.
             </p>
             <div className="flex items-center gap-8">
               <div className="flex items-center">
                 <FaUsers className="text-4xl text-secondary mr-3" />
-                <p className="text-xl font-semibold">Community Driven</p>
+                <p className="text-xl font-semibold">Tech Enthusiast</p>
               </div>
               <div className="flex items-center">
                 <FaCode className="text-4xl text-secondary mr-3" />
-                <p className="text-xl font-semibold">Tech Experts</p>
+                <p className="text-xl font-semibold">Problem Solver</p>
               </div>
             </div>
           </div>
-          <div className="flex-1 border border-accent p-1 rounded-lg">
+          <div className="flex-1 flex items-center justify-center border border-accent p-1 rounded-lg">
             <img
-              src={Team}
-              alt="Tech Team"
-              className="w-full h-full object-cover rounded-lg shadow-md"
+              src={ProfileImageLarge}
+              alt="Personal Image"
+              className="w-1/2 h-auto object-fit rounded-lg"
             />
           </div>
         </div>
@@ -73,17 +88,25 @@ const AboutUs = () => {
             theme === "light" ? "bg-gray-100" : "bg-neutral-800 text-white"
           } text-gray-700 py-24 relative`}
         >
-          <div className="relative z-10 text-center">
+          <div className="relative z-10 text-center px-1.5 md:px-0">
             <h2 className="text-4xl font-extrabold mb-4">The Challenge</h2>
             <p className="text-lg max-w-2xl mx-auto mb-8">
               Staying updated with the fast-paced world of tech can be
               overwhelming. Developers often struggle to find reliable and
               digestible content that covers the most recent trends and
-              innovations. We understood this struggle and set out to create a
+              innovations. I understood this struggle and set out to create a
               space that addresses these gaps.
             </p>
             <div className="flex justify-center gap-6">
-              <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-6 rounded-lg">
+              <button
+                className="button"
+                onClick={() =>
+                  handleButtonClick(
+                    "Feature Maintainance",
+                    "Thank you for your interest! We're working on enhancing this feature.Once ready, you'll be able to participate in insightful discussions with developers worldwide, sharing ideas and building connections.."
+                  )
+                }
+              >
                 Join the Conversation
               </button>
             </div>
@@ -99,17 +122,25 @@ const AboutUs = () => {
               className="w-full h-full object-cover opacity-60"
             />
           </div>
-          <div className="relative z-10 text-center text-white">
-            <h2 className="text-4xl font-extrabold mb-4">Our Solution</h2>
+          <div className="relative z-10 text-center text-white px-1.5 md:px-0">
+            <h2 className="text-4xl font-extrabold mb-4">My Solution</h2>
             <p className="text-lg mb-8 max-w-3xl mx-auto">
-              We deliver high-quality, accessible content that helps developers
-              stay ahead of industry changes. From detailed tutorials to
-              industry insights, we provide content that is actionable,
-              inspiring, and informative. Our mission is to empower developers
-              to not only keep up but to lead in the tech world.
+              In this blog i deliver high-quality, accessible content that helps
+              developers stay ahead of industry changes. From detailed tutorials
+              to industry insights, I provide content that is actionable,
+              inspiring, and informative. My mission is to empower developers to
+              not only keep up but to lead in the tech world.
             </p>
             <div className="flex justify-center gap-6">
-              <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-6 rounded-lg">
+              <button
+                className="button"
+                onClick={() =>
+                  handleButtonClick(
+                    "Feature Maintainance",
+                    "Our content is still under maintenance. Soon, you'll have access to detailed tutorials, industry insights, and actionable tips to empower your development journey."
+                  )
+                }
+              >
                 Discover Our Content
               </button>
             </div>
@@ -122,22 +153,36 @@ const AboutUs = () => {
             theme === "light" ? "bg-gray-100" : "bg-neutral-800 text-white"
           } py-24`}
         >
-          <div className="text-center">
-            <h2 className="text-4xl font-extrabold mb-4">What’s Next for Us</h2>
+          <div className="text-center px-1.5 md:px-0">
+            <h2 className="text-4xl font-extrabold mb-4">What’s Next for Me</h2>
             <p className="text-lg  max-w-3xl mx-auto mb-8">
-              Our journey is far from over. We’re continuously evolving,
-              collaborating with industry leaders, and planning new initiatives
-              to foster a thriving community of tech professionals. Stay tuned
-              for upcoming projects, events, and opportunities.
+              My journey is ongoing. I’m committed to learning, collaborating,
+              and building projects that make a difference. The future holds
+              exciting possibilities, and I am eager to embrace them.
             </p>
             <div className="flex justify-center gap-6">
-              <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-6 rounded-lg">
+              <button
+                className="button"
+                onClick={() =>
+                  handleButtonClick(
+                    "Feature Maintainance",
+                    "Our mailing list sign-up is under maintenance. Once operational, you’ll receive exclusive insights, the latest tech trends, and updates directly in your inbox."
+                  )
+                }
+              >
                 Join Our Mailing List
               </button>
             </div>
           </div>
         </div>
       </div>
+      {/* Popup Component */}
+      <Popup
+        isOpen={isPopupOpen}
+        onClose={closePopup}
+        title={popupTitle}
+        message={popupMessage}
+      />
     </section>
   );
 };
